a3a970694db2d5a30efa193c90581d659b1575e5
T40N <b.ratajczyk124@gmail.com>
2021-04-14 16:46:56 +0200

    clearing 2

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,17 +1,6 @@
 import React, { useEffect, useState } from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import {
-  createStyles,
-  fade,
-  Theme,
-  makeStyles,
-} from "@material-ui/core/styles";
-import SearchIcon from "@material-ui/icons/Search";
 import axios from "axios";
-import { Input } from "@material-ui/core";
 import { TopBar } from "./main/topBar";
 import { PostGrid } from "./main/postGrid";
 import { useStyles } from "./main/styles";
